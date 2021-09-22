1227cbce4d483a0ac8d2a77a3dcbc4c114c9260e
jesseburke <jtb445@gmail.com>
2021-09-22 18:45:41 +1000

    Initial commit

---
+++ App_fg.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+
+import { Provider as JProvider } from 'jotai';
+
+import * as THREE from 'three';
+
+import { OptionsTabComp } from '@jesseburke/components';
+
+import { ThreeSceneComp } from '@jesseburke/three-scene-with-react';
+import { MainDataComp } from '@jesseburke/jotai-data-setup';
+import { Grid } from '@jesseburke/three-scene-with-react';
+import { Axes3D } from '@jesseburke/three-scene-with-react';
+import { FunctionGraph3D } from '@jesseburke/three-scene-with-react';
+import { CameraControls } from '@jesseburke/three-scene-with-react';
+
+import {
+    funcData,
+    boundsData,
+    gridBoundsAtom,
+    labelData,
+    cameraData,
+    axesData,
+    atomStoreAtom
+} from './App_fg_atoms';
+
+const initColors = {
+    arrows: '#C2374F',
+    solution: '#C2374F',
+    firstPt: '#C2374F',
+    secPt: '#C2374F',
+    testFunc: '#E16962', //#DBBBB0',
+    axes: '#0A2C3C',
+    controlBar: '#0A2C3C',
+    clearColor: '#f0f0f0',
+    funcGraph: '#E53935'
+};
+
+const initControlsData = {
+    mouseButtons: { LEFT: THREE.MOUSE.ROTATE },
+    touches: { ONE: THREE.MOUSE.PAN, TWO: THREE.TOUCH.DOLLY, THREE: THREE.MOUSE.ROTATE },
+    enableRotate: true,
+    enablePan: true,
+    enabled: true,
+    keyPanSpeed: 50,
+    screenSpaceSpanning: false
+};
+
+const aspectRatio = window.innerWidth / window.innerHeight;
+
+const fixedCameraData = {
+    up: [0, 0, 1],
+    near: 0.1,
+    far: 1000,
+    aspectRatio,
+    orthographic: false
+};
+
+const btnClassStr =
+    'absolute left-8 p-2 border med:border-2 rounded-md border-solid border-persian_blue-900 bg-gray-200 cursor-pointer text-lg';
+
+const saveBtnClassStr = btnClassStr + ' bottom-40';
+
+const resetBtnClassStr = btnClassStr + ' bottom-24';
+
+const photoBtnClassStr = btnClassStr + ' bottom-8';
+
+//------------------------------------------------------------------------
+
+export default function App() {
+    const DataComp = MainDataComp(atomStoreAtom);
+
+    return (
+        <JProvider>
+            <div className='full-screen-base'>
+                <header
+                    className='control-bar bg-persian_blue-900 font-sans
+		    p-8 text-white'
+                >
+                    <funcData.component />
+                    <OptionsTabComp
+                        className={'w-32 bg-gray-50 text-persian_blue-900 p-2 rounded'}
+                        nameComponentArray={[
+                            ['Axes', axesData.component],
+                            ['Bounds', boundsData.component],
+                            ['Camera', cameraData.component],
+                            ['Variable labels', labelData.component]
+                        ]}
+                    />
+                </header>
+
+                <main className='flex-grow relative p-0'>
+                    <ThreeSceneComp
+                        fixedCameraData={fixedCameraData}
+                        controlsData={initControlsData}
+                        photoButton={true}
+                        photoBtnClassStr={photoBtnClassStr}
+                    >
+                        <Axes3D
+                            tickDistance={1}
+                            boundsAtom={gridBoundsAtom}
+                            axesDataAtom={axesData.atom}
+                            labelAtom={labelData.atom}
+                        />
+                        <Grid boundsAtom={gridBoundsAtom} gridShow={true} />
+                        <FunctionGraph3D
+                            funcAtom={funcData.funcAtom}
+                            boundsAtom={boundsData.atom}
+                            color={initColors.funcGraph}
+                        />
+                        <CameraControls cameraDataAtom={cameraData.atom} />
+                    </ThreeSceneComp>
+                    <DataComp
+                        resetBtnClassStr={resetBtnClassStr}
+                        saveBtnClassStr={saveBtnClassStr}
+                    />
+                </main>
+            </div>
+        </JProvider>
+    );
+}
---
+++ App_fg_atoms.ts
@@ -0,0 +1,95 @@
+import { atom, useAtom } from 'jotai';
+
+import { LabelDataComp } from '@jesseburke/jotai-data-setup';
+import { FunctionDataComp } from '@jesseburke/jotai-data-setup';
+import { AxesDataComp } from '@jesseburke/jotai-data-setup';
+import { BoundsDataComp } from '@jesseburke/jotai-data-setup';
+import { PerspCameraData } from '@jesseburke/jotai-data-setup';
+
+import { Bounds } from '../../my-types';
+
+//------------------------------------------------------------------------
+//
+// initial constants
+
+const colors = {
+    arrows: '#B01A46', //'#C2374F'
+    solutionCurve: '#4e6d87', //'#C2374F'
+    tick: '#e19662'
+};
+
+const initFuncStr = 'x*y*sin(x+y)/10';
+
+const initAxesData = {
+    radius: 0.01,
+    tickRadiusMultiple: 20,
+    show: true,
+    tickLabelDistance: 0
+};
+
+const initBounds: Bounds = { xMin: -20, xMax: 20, yMin: -20, yMax: 20, zMin: -20, zMax: 20 };
+
+const initCameraData = {
+    target: [0, 0, 0],
+    position: [29, 64, 36]
+};
+
+export const labelStyle = {
+    color: 'black',
+    padding: '.1em',
+    margin: '.5em',
+    fontSize: '1.5em'
+};
+
+const tickLabelStyle = Object.assign(Object.assign({}, labelStyle), {
+    fontSize: '1em',
+    color: colors.tick
+});
+
+//------------------------------------------------------------------------
+//
+// primitive atoms
+
+export const labelData = LabelDataComp();
+export const axesData = AxesDataComp({
+    ...initAxesData,
+    tickLabelStyle
+});
+
+export const funcData = FunctionDataComp({
+    initVal: initFuncStr,
+    functionLabelAtom: atom(
+        (get) => 'f(' + get(labelData.atom).x + ',' + get(labelData.atom).y + ') = '
+    ),
+    labelAtom: labelData.atom
+});
+
+export const boundsData = BoundsDataComp({
+    initBounds,
+    labelAtom: labelData.atom
+});
+
+const gridOverhang = 10;
+
+export const gridBoundsAtom = atom((get) => {
+    const { xMin, xMax, yMin, yMax, zMin, zMax } = get(boundsData.atom);
+
+    return {
+        xMin: xMin - gridOverhang,
+        xMax: xMax + gridOverhang,
+        yMin: yMin - gridOverhang,
+        yMax: yMax + gridOverhang,
+        zMin: zMin - gridOverhang,
+        zMax: zMax + gridOverhang
+    };
+});
+
+export const cameraData = PerspCameraData(initCameraData);
+
+export const atomStoreAtom = atom({
+    ls: labelData.readWriteAtom,
+    ax: axesData.readWriteAtom,
+    fn: funcData.readWriteAtom,
+    bd: boundsData.readWriteAtom,
+    cd: cameraData.readWriteAtom
+});
